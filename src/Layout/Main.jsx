256cf6c912ea2146f1e2bdcb0ba84a02bca47c29
SamiulHasan-dev <samiul.199897@gmail.com>
2024-06-10 20:59:13 +0600

    footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Shared/NavBar";
+import Footer from "../pages/Footer/Footer";
 
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
         <div>
             <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
